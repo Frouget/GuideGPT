67f5e0d5380843f9bb4bfbe6a2cd471ab675aa20
Frouget <80108514+Frouget@users.noreply.github.com>
2024-04-25 15:00:41 +0100

    Update signIn.js

--- FrontEnd/signIn.js
+++ FrontEnd/signIn.js
@@ -1,12 +1,16 @@
 window.addEventListener("load", function (evt) {
+    
+    //Function for toggling which form is being shown
     function toggleForm() {  
         logInForm.classList.toggle('active');
         signUpForm.classList.toggle('active');
     }
-
+    
+    //Function for sending a login request to the API
     async function loginUser(username, password) {
         try {
-            const response = await fetch('https://fgr11.brighton.domains/testing/guidegpt/api.php/logIn', {
+            //Building the request
+            const loginResponse = await fetch('https://fgr11.brighton.domains/testing/guidegpt/api.php/logIn', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -17,34 +21,37 @@ window.addEventListener("load", function (evt) {
                 })
             });
     
-            if (!response.ok) {
-                const errorMessage = await response.text();
+            //Checking the response for errors
+            if (!loginResponse.ok) {
+                const errorMessage = await loginResponse.text();
                 throw new Error(errorMessage);
             }
-    
-            const data = await response.json();
-            if(data.success != null && data.success === true) {
-                const sessionId = data.pid; // Assuming the API sends back an ID upon successful login
-    
-                // Storing the session ID in the user's session
+            
+            const loginData = await loginResponse.json();
+            //Checking data was successful and contains a PID
+            if(loginData.success && loginData.pid != null) {
+                //Assigning the session ID to the user's session storage
+                const sessionId = loginData.pid;
                 sessionStorage.setItem('sessionId', sessionId);
-
-                // Redirecting the user to the home page
+                //Redirecting the user to the home page
                 window.location.href = 'home.html';
             }else {
-                alert("Error: " + data.error);
+                //Presenting errors to the user if they occur
+                alert("Error: " + loginData.error);
             }
             
     
         } catch (error) {
-            // Display error message to the user
+            //Display error message to the user
             console.error('Login failed:', error.message);
         }
     }
 
+    //Function for sending a signup request to the API
     async function signUpUser(email, name, username, password) {
         try {
-            const response = await fetch('https://fgr11.brighton.domains/testing/guidegpt/api.php/signUp', {
+            //Building the request
+            const signUpResponse = await fetch('https://fgr11.brighton.domains/testing/guidegpt/api.php/signUp', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -56,37 +63,44 @@ window.addEventListener("load", function (evt) {
                     "password": password
                 })
             });
-    
-            if (!response.ok) {
-                const errorMessage = await response.text();
+            
+            //Checking the response for errors
+            if (!signUpResponse.ok) {
+                const errorMessage = await signUpResponse.text();
                 throw new Error(errorMessage);
             }
-    
-            const data = await response.json();
-            const sessionId = data.pid// Assuming the API sends back an ID upon successful login
-    
-            // Storing the session ID in the user's session
-            sessionStorage.setItem('sessionId', sessionId);
             
-            // Redirecting the user to the home page
-            window.location.href = 'home.html';
+            const signUpData = await signUpResponse.json();
+            //Checking data was successful and contains a PID
+            if(signUpData.success && signUpData.pid != null) {
+                //Assigning the session ID to the user's session storage
+                const sessionId = signUpData.pid;
+                sessionStorage.setItem('sessionId', sessionId);
+                //Redirecting the user to the home page
+                window.location.href = 'home.html';
+            }else {
+                //Presenting errors to the user if they occur
+                alert("Error: " + signUpData.error);
+            }
             
         } catch (error) {
-            // Display error message to the user
+            //Display error message to the user
             console.error('Login failed:', error.message);
-            // You can modify HTML to display the error message to the user here
         }
     }
     
+    //Function to handle processing when a user clicks a submit button
     async function handleFormSubmit(event) {
-        event.preventDefault(); // Prevent the form from submitting normally
+        event.preventDefault();
         
+        //Defining and retrieving variables
         const form = event.target;
         const formData = new FormData(form);
         
         const username = formData.get('username');
         const password = formData.get('password');
         
+        //Checking if it is a login or signup request and launching the correct function
         if (form.id === 'logInForm') {
             await loginUser(username, password);
         } else if (form.id === 'signUpForm') {
